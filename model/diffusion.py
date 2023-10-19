f0e8e3f2f090d0bf67efd1799fa8d69cefff2c19
CarlosMari <97597629+CarlosMari@users.noreply.github.com>
2023-10-19 13:28:57 -0500

    Cosine Scheduler Maybe?

--- model/diffusion.py
+++ model/diffusion.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
 from PIL import Image
-
+import math 
 class GaussianDiffusion:
   def __init__(self, model, noise_steps, beta_0, beta_T, image_size, channels=3):
     """
@@ -33,7 +33,33 @@ class GaussianDiffusion:
   def beta_schedule(self, schedule="linear"):
     if schedule == "linear":
       return torch.linspace(self.beta_0, self.beta_T, self.noise_steps).to(self.device)
+    elif schedule == "cosine":
+        return self.betas_for_alpha_bar(
+            self.noise_steps,
+            lambda t: math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2,
+        )
+      
+
+  def betas_for_alpha_bar(num_diffusion_timesteps, alpha_bar, max_beta=0.999):
+    """
+    Create a beta schedule that discretizes the given alpha_t_bar function,
+    which defines the cumulative product of (1-beta) over time from t = [0,1].
+
+    :param num_diffusion_timesteps: the number of betas to produce.
+    :param alpha_bar: a lambda that takes an argument t from 0 to 1 and
+                      produces the cumulative product of (1-beta) up to that
+                      part of the diffusion process.
+    :param max_beta: the maximum beta to use; use values lower than 1 to
+                     prevent singularities.
+    """
+    betas = []
+    for i in range(num_diffusion_timesteps):
+        t1 = i / num_diffusion_timesteps
+        t2 = (i + 1) / num_diffusion_timesteps
+        betas.append(min(1 - alpha_bar(t2) / alpha_bar(t1), max_beta))
+    return np.array(betas)
   
+
   def sample_time_steps(self, batch_size=1):
     return torch.randint(0, self.noise_steps, (batch_size,)).to(self.device)
   
