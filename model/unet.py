ddf99dae454beecb1a181cc6d009ee928680332f
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-10-24 09:48:59 -0500

    zero module and attention change

--- model/unet.py
+++ model/unet.py
@@ -2,6 +2,9 @@ from typing import Any
 import torch
 import torch.nn as nn
 from collections import defaultdict
+import torch as th
+import numpy as np
+import math
 
 str_to_act = defaultdict(lambda: nn.ReLU())
 str_to_act.update({
@@ -10,6 +13,13 @@ str_to_act.update({
     "gelu": nn.GELU(),
 })
 
+def zero_module(module):
+    """
+    Zero out the parameters of a module and return it.
+    """
+    for p in module.parameters():
+        p.detach().zero_()
+    return module
 
 class SinusoidalPositionalEncoding(nn.Module):
     def __init__(self, dim):
@@ -41,7 +51,7 @@ class TimeEmbedding(nn.Module):
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, act="relu", dropout=None):
+    def __init__(self, in_channels, out_channels, act="relu", dropout=None, zero=False):
         super().__init__()
 
         self.norm = nn.GroupNorm(
@@ -60,6 +70,8 @@ class ConvBlock(nn.Module):
             kernel_size=3,
             padding=1,
         )
+        if zero:
+            self.conv = zero_module(self.conv)
 
         
     def forward(self, x):
@@ -95,7 +107,7 @@ class ResBlock(nn.Module):
         self.emb = EmbeddingBlock(out_channels, emb_dim) 
 
         #self.dropout = nn.Dropout(dropout)
-        self.conv2 = ConvBlock(out_channels, out_channels, dropout=dropout)
+        self.conv2 = ConvBlock(out_channels, out_channels, dropout=dropout, zero=True)
 
         if channels != out_channels:
             self.skip_connection = nn.Conv2d(channels, out_channels, kernel_size=1)
@@ -121,6 +133,80 @@ class ResBlock(nn.Module):
         return x
 
 class SelfAttentionBlock(nn.Module):
+    """
+    An attention block that allows spatial positions to attend to each other.
+
+    Originally ported from here, but adapted to the N-d case.
+    https://github.com/hojonathanho/diffusion/blob/1e0dceb3b3495bbe19116a5e1b3596cd0706c543/diffusion_tf/models/unet.py#L66.
+    """
+
+    def __init__(self, channels, num_heads=4, use_checkpoint=False):
+        super().__init__()
+        self.channels = channels
+        self.num_heads = num_heads
+        self.use_checkpoint = use_checkpoint
+
+        self.norm = nn.GroupNorm(32, channels)
+        self.qkv = nn.Conv1d(channels, channels * 3, 1)
+        self.attention = QKVAttention()
+        self.proj_out = zero_module(nn.Conv1d(channels, channels, 1))
+
+    def forward(self, x):
+        b, c, *spatial = x.shape
+        x = x.reshape(b, c, -1)
+        qkv = self.qkv(self.norm(x))
+        qkv = qkv.reshape(b * self.num_heads, -1, qkv.shape[2])
+        h = self.attention(qkv)
+        h = h.reshape(b, -1, h.shape[-1])
+        h = self.proj_out(h)
+        return (x + h).reshape(b, c, *spatial)
+
+
+class QKVAttention(nn.Module):
+    """
+    A module which performs QKV attention.
+    """
+
+    def forward(self, qkv):
+        """
+        Apply QKV attention.
+
+        :param qkv: an [N x (C * 3) x T] tensor of Qs, Ks, and Vs.
+        :return: an [N x C x T] tensor after attention.
+        """
+        ch = qkv.shape[1] // 3
+        q, k, v = th.split(qkv, ch, dim=1)
+        scale = 1 / math.sqrt(math.sqrt(ch))
+        weight = th.einsum(
+            "bct,bcs->bts", q * scale, k * scale
+        )  # More stable with f16 than dividing afterwards
+        weight = th.softmax(weight.float(), dim=-1).type(weight.dtype)
+        return th.einsum("bts,bcs->bct", weight, v)
+
+    @staticmethod
+    def count_flops(model, _x, y):
+        """
+        A counter for the `thop` package to count the operations in an
+        attention operation.
+
+        Meant to be used like:
+
+            macs, params = thop.profile(
+                model,
+                inputs=(inputs, timestamps),
+                custom_ops={QKVAttention: QKVAttention.count_flops},
+            )
+
+        """
+        b, c, *spatial = y[0].shape
+        num_spatial = int(np.prod(spatial))
+        # We perform two matmuls with the same number of ops.
+        # The first computes the weight matrix, the second computes
+        # the combination of the value vectors.
+        matmul_ops = 2 * b * (num_spatial ** 2) * c
+        model.total_ops += th.DoubleTensor([matmul_ops])
+
+class SelfAttentionBlockOld(nn.Module):
     def __init__(self, channels: int):
         super().__init__()
         self.transformer = nn.TransformerEncoderLayer(
@@ -376,12 +462,12 @@ class Unet(nn.Module):
         self.head = nn.Sequential(
             nn.GroupNorm(32, starting_channels),
             nn.ReLU(),
-            nn.Conv2d(
+            zero_module(nn.Conv2d(
                 in_channels=starting_channels,
                 out_channels=image_channels,
                 kernel_size=3,
                 padding=1,
-            )
+            ))
         )
 
     def forward(self, x, t):
