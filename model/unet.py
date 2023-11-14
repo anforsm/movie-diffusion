5eb19d324fcac94c0cacdffcd4cffbe92e773f93
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-11-14 00:01:36 -0600

    removed zero module

--- model/unet.py
+++ model/unet.py
@@ -19,8 +19,8 @@ def zero_module(module):
     """
     Zero out the parameters of a module and return it.
     """
-    for p in module.parameters():
-        p.detach().zero_()
+    #for p in module.parameters():
+    #    p.detach().zero_()
     return module
 
 class SinusoidalPositionalEncoding(nn.Module):
